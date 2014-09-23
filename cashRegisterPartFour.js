cd672c7229c502d93ba514e7c378a6a650e3f258
Damian Esteban <damian.esteban@gmail.com>
2014-09-23 18:09:43 -0400

    all exercises finished

--- cashRegisterPartFour.js
+++ cashRegisterPartFour.js
@@ -1,7 +1,7 @@
 // We need to figure out a way to apply the differenr
 // levels of discounts to students
 // Create a constructor for the staffMember class
-function StaffMember(new, discountPercent) {
+function StaffMember(name, discountPercent) {
   this.name = name;
   this.discountPercent = discountPercent;
 }
@@ -11,3 +11,5 @@ var sally = new StaffMember("Sally", 15);
 var bob = new StaffMember("Bob", 23);
 var satan = new StaffMember("Lucifer", 100);
 
+// Create yourself
+var me = new StaffMember("Superman", 52);
